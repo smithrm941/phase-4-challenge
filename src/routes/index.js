857d71b6171ba94b294578d44a7164240cc1f694
smithrm941 <smithrm941@gmail.com>
2017-10-03 02:12:22 -0700

    linked pages to css, js files, trash can is on proper page

--- src/routes/index.js
+++ src/routes/index.js
@@ -33,20 +33,19 @@ router.get('/albums/:albumID', (req, res) => {
 router.get('/albums/:albumID/new_review', (req, res) => {
   const albumID = req.params.albumID
   db.getAlbumsByID(albumID, (error, albums) => {
-    const album = albums[0]
-    console.log('pressssed the add review button::::::', album)
-    res.render('new_review', {album})
+    if (error) {
+      res.status(500).render('error', {error})
+    } else {
+      const album = albums[0]
+      const {newReviewContent} = req.body
+      res.render('new_review', {album})
+    }
   })
-  // db.getAlbumsByID(albumID, (error, albums) => {
-  //   if (error) {
-  //     res.status(500).render('error', {error})
-  //   } else {
-  //     console.log('just pressed the add review button', albums)
-  //     const album = albums[0]
-  //     res.render('new_review', {album})
-  //   }
-  // })
 })
+
+//route to submit the new review:::
+//router.post('/albums/:albumID/new_review')
+//this includes req.body stuff
 
 //trash button would make a modal pop up and
 //this would be triggered by button on delete confirmation modal:
@@ -76,27 +75,5 @@ router.get('/users/:userID', (req, res) => {
   })
 })
 
-
-
-
-// router.post('/albums/:albumID/new_review', (req, res) => {
-//   const albumID = req.params.albumID
-//   const {content} = req.body
-//
-//   db.getAlbumsByID(albumID, (error, albums) => {
-//     if (error) {
-//       res.status(500).render('error', {error})
-//     } else {
-//       const album = albums[0]
-//       if(album) {
-//         //put query for adding review to database here
-//         db.getReviewsByAlbumID(album.id, (error, reviews) => {
-//           res.render('album', {album, reviews})
-//         })
-//       } else {
-//         res.status(404).render('not_found')
-//       }
-//     }
-// })
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ require('ejs')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/', routes)
